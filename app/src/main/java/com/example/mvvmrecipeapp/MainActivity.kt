66fa2e5bfb05a8280ef52608f801ccb52477403c
Okekeprince1 <okeke.p.ebube@gmail.com>
2021-04-23 06:47:15 +0100

    added order now button

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -7,20 +7,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.mvvmrecipeapp.ui.theme.MVVMRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +38,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting() {
     Column (
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .background(
                 color = Color(0xFFF2F2F2)
             )
@@ -50,22 +49,36 @@ fun Greeting() {
             painter = painterResource(id = R.drawable.happy_meal),
             contentDescription = "Happy meal",
             modifier = Modifier.height(300.dp),
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
             )
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
-            Text(
-                text = "French Fries",
-                style = typography.h5
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Text(
+                    text = "French Fries",
+                    style = typography.h5,
                 )
+                Text(text = "$ 80",
+                    color = Color(0xFFA1FA39),
+                    style = typography.h6,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
             Spacer(modifier = Modifier.padding(top = 8.dp))
             Text(text = "200 Calories",
-                style = typography.h6)
-            Spacer(modifier = Modifier.padding(top = 8.dp))
-            Text(text = "$ 80",
-                color = Color.Green,
-                style = typography.h6)
+                style = typography.h6,
+                color = Color(0xFF5F5F5F))
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+                    .background(color = Color(0xFFFCF3E70))
+            ) {
+                Text(text = "ORDER NOW")
+            }
         }
     }
 
