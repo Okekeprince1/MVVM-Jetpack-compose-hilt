816c362b36ee8b668e7601e3975e2398705bab96
Okekeprince1 <okeke.p.ebube@gmail.com>
2021-04-27 13:40:38 +0100

    completed navigation

---
+++ app/src/main/java/com/example/mvvmrecipeapp/RecipeFragment.kt
@@ -0,0 +1,44 @@
+package com.example.mvvmrecipeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+
+class RecipeFragment : Fragment ( ) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme {
+                    Recipe()
+
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Recipe () {
+    Text(text = "RECIPE FRAGMENT")
+}
